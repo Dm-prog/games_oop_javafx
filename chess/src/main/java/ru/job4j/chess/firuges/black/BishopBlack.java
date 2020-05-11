cbb72b22d87209fb19f1e13d6a92c8d515a974a8
Dm-prog <53370362+Dm-prog@users.noreply.github.com>
2020-05-11 15:36:19 +0400

    2. Каркас шахматной доски[#251103]

--- chess/src/main/java/ru/job4j/chess/firuges/black/BishopBlack.java
+++ chess/src/main/java/ru/job4j/chess/firuges/black/BishopBlack.java
@@ -17,12 +17,27 @@ public class BishopBlack implements Figure {
 
     @Override
     public Cell[] way(Cell source, Cell dest) {
-        throw new IllegalStateException(
-                String.format("Could not way by diagonal from %s to %s", source, dest)
-        );
+        if (!isDiagonal(source, dest)) {
+            throw new IllegalStateException(
+                    String.format("Could not move by diagonal from %s to %s", source, dest)
+            );
+        }
+        int size = 8;
+        Cell[] steps = new Cell[size];
+        int deltaX = -1;
+        int deltaY = 1;
+        for (int index = 0; index < size; index++) {
+            int x = index + deltaX;
+            int y = index + deltaY;
+            steps[index] = Cell.findBy(x, y);
+        }
+        return steps;
     }
 
     public boolean isDiagonal(Cell source, Cell dest) {
+        if ((source.x - dest.x) == 0 && (source.y - dest.y) == 0) {
+            return true;
+        }
         return false;
     }
 
--- chess/src/test/java/ru/job4j/chess/BishopBlackTest.java
+++ chess/src/test/java/ru/job4j/chess/BishopBlackTest.java
@@ -1,4 +1,27 @@
 package ru.job4j.chess;
 
+import org.junit.Test;
+import ru.job4j.chess.firuges.Cell;
+import ru.job4j.chess.firuges.black.BishopBlack;
+
 public class BishopBlackTest {
+    @Test
+    public void position() {
+
+    }
+
+    @Test
+    public void way() {
+
+    }
+
+    @Test
+    public void isDiagonal() {
+
+    }
+
+    @Test
+    public void copy() {
+
+    }
 }
